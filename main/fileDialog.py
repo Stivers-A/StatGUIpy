181ff31551a3c1823b2fa50eea3ecd21fca09cf3
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-12-05 18:19:46 -0800

    Made main GUI and set up basic grid need to add widgets and fill out its shape

--- main/fileDialog.py
+++ main/fileDialog.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from tkinter import filedialog
 window = tk.Tk()
-window.wm_attributes('-topmost', 1)
+window.wm_attributes('-topmost', 1) 
 window.withdraw()   # this supress the tk window
 
 file_name = filedialog.askopenfilename(
     parent=window,
     initialdir="",
     title="Select A File",
+    #this automatically closes after opening the file finder gui of the OS
     filetypes = (("Read Me", "*.md"), ("All files", "*")))
+    #This dictates the files that show up
 # window.wm_attributes('-topmost', 1) and "parent=window" argument help open the dialog box on top of other windows
 print(file_name)
