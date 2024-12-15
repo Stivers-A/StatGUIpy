2f23084b936a1ba447355b0d5ee84455f7a522f3
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-12-15 15:57:30 -0800

    started setup for doing stuff with a file still need a toggle to select row or column

--- main/fileDialog.py
+++ main/fileDialog.py
@@ -9,11 +9,10 @@ def file_opener():
         initialdir="",
         title="Select A File",
         #this automatically closes after opening the file finder gui of the OS
-        filetypes = (("Read Me", "*.md"), ("All files", "*")))
+        filetypes = (("Spreadsheets", "*.csv"), ("All files", "*")))
         
     print(file_name ,"print from fileDialog")
     file_name_output = str(file_name)
     return file_name_output
     #This dictates the files that show up
 # window.wm_attributes('-topmost', 1) and "parent=window" argument help open the dialog box on top of other windows
-file_opener()
--- main/mainGUI.py
+++ main/mainGUI.py
@@ -2,13 +2,21 @@ from tkinter import *
 from tkinter import ttk
 import sv_ttk
 from fileDialog import file_opener
+import csv
 
-file_name = " "
+
+file_name = ""
 def fileSelect():
     global file_name 
     file_name = file_opener()
-    print(file_name, "print from mainGUI")
-    
+    print(file_name, "print from fileSelect")
+    #uses file_opener in fileDialog to save the path to a file as file_name
+def confirmSelect():
+    global file_name
+    print(file_name, "print from confirmSelect")
+        
+
+
 root = Tk()
 root.title("Stats GUI  Py")
 mainframe = ttk.Frame(root, padding="3 3 12 12")
@@ -18,11 +26,16 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 ttk.Button(mainframe, text="Select File", command=fileSelect).grid(column=1, row=1, sticky=N)
-ttk.Button(mainframe, text="Box").grid(column=9, row=1, sticky=N)
+ttk.Button(mainframe, text="Confirm Selection",command=confirmSelect).grid(column=9, row=1, sticky=N)
+
+bar = StringVar()
+bar_entry = ttk.Entry(mainframe, width=25, textvariable=bar)
+bar_entry.grid(column=3, row=2, sticky=(W, E))
+#first creates a string for feet, then creates an entry text box that assigns its input to feet
 ttk.Button(mainframe, text="Box2").grid(column=8, row=9, sticky=N)
 
 
-ttk.Label(mainframe, text="").grid(column=2, row=2, sticky=(W, E))
+ttk.Label(mainframe, text="Barname Name").grid(column=2, row=2, sticky=(W, E))
 
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
